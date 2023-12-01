d28cbdec7bea3dc55514151b1de8507d03b11057
Peter Regier <peter.regier@pnnl.gov>
2023-12-01 08:35:16 -0700

    add scripts and initial figures

--- scripts/0_setup.R
+++ scripts/0_setup.R
@@ -3,6 +3,36 @@
 require(pacman)
 p_load(tidyverse,
        cowplot,
+       scales, #trans_format()
        PNWColors)
 
 theme_set(theme_bw())
+
+## Core dataset that's used for most things, so load it
+scaling_analysis_dat <- read_csv("data/231008_scaling_analysis_dat.csv")
+
+## Plotting details baked into Fco functions
+
+# Creating breaks for logarithmic scale 
+# (see: https://r-graphics.org/recipe-axes-axis-log)
+
+breaks <- 10^(-10:20)
+breaks_c <- 10^seq(-10,20,by=4)
+minor_breaks <- rep(1:9, 31)*(10^rep(-10:20, each=9))
+
+theme_httn<-  theme(axis.text=element_text(colour="black",size=22),
+                    axis.title = element_text(size = 32, face = "bold"),
+                    panel.grid.minor= element_line(colour = "gray", linetype = "dotted"), 
+                    panel.grid.major = element_line(colour = "gray", linetype = "dashed"),
+                    panel.border = element_rect(fill=NA, colour = "black", linewidth = 1.5),
+                    panel.background=element_rect(fill="white"),
+                    axis.ticks.length = unit(0.254, "cm"),
+                    axis.ticks = element_line(colour = "black", linewidth = 1), 
+                    axis.line = element_line(colour = "black"),
+                    legend.position = c(0.85,0.25),
+                    legend.direction = "vertical",
+                    legend.background = element_blank(),
+                    legend.key.size = unit(1.0, 'lines'),#Changing spacing between legend keys
+                    legend.title = element_text())
+
+
--- scripts/231130_watershed_characteristics.R
+++
@@ -1,6 +0,0 @@
-## This script takes Fco's code from https://github.com/Scaling-Watershed-Function/2-swf-analytical.engine
-## as of 11/30/23 and recreates figures so I can edit for AGU poster
-##
-
-source("scripts/0_setup.R")
-
